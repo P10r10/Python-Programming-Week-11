39c52c65226648f53bf9e0f2f305f39e1add734a
P10r10 <asantiago@keemail.me>
2023-06-30 15:27:07 +0100

    several

---
+++ filter_forbidden.py
@@ -0,0 +1,8 @@
+def filter_forbidden(string: str, forbidden: str) -> str:
+    return "".join([ch for ch in string if ch not in forbidden])
+
+
+if __name__ == "__main__":
+    sentence = "Once! upon, a time: there was a python!??!?!"
+    filtered = filter_forbidden(sentence, "!?:,.")
+    print(filtered)
